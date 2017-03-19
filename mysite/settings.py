61d571ef63764aa53f9c42478e937aa63d3f77d2
psycho-mushroom <slowpoke02@gmail.com>
2017-03-19 08:22:40 +0200

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'hr8%qq)&-w$_-(u!x^h9rk5#@2$c88qsb8dwa7wcxi#qw-xtd+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['enor.pythonanywhere.com']
 
 
 # Application definition
